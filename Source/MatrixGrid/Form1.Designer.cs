5d345c45a6c64e5d496026b5d187c2ec34a0e0f7
Tomin Jose <tominjose111@gmail.com>
2021-02-02 13:27:23 -0800

    Commit

---
+++ Source/MatrixGrid/Form1.Designer.cs
@@ -0,0 +1,169 @@
+﻿using System.Threading;
+namespace MatrixGrid
+{
+    partial class Form1
+    {
+        /// <summary>
+        /// Required designer variable.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Clean up any resources being used.
+        /// </summary>
+        /// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form Designer generated code
+
+        /// <summary>
+        /// Required method for Designer support - do not modify
+        /// the contents of this method with the code editor.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.dataGridView1 = new System.Windows.Forms.DataGridView();
+            this.dataGridView2 = new System.Windows.Forms.DataGridView();
+            this.menuStrip1 = new System.Windows.Forms.MenuStrip();
+            this.threadToolStripMenuItem = new System.Windows.Forms.ToolStripMenuItem();
+            this.startToolStripMenuItem = new System.Windows.Forms.ToolStripMenuItem();
+            this.pauseToolStripMenuItem = new System.Windows.Forms.ToolStripMenuItem();
+            this.resumeToolStripMenuItem = new System.Windows.Forms.ToolStripMenuItem();
+            this.button1 = new System.Windows.Forms.Button();
+            this.Stop = new System.Windows.Forms.Button();
+            ((System.ComponentModel.ISupportInitialize)(this.dataGridView1)).BeginInit();
+            ((System.ComponentModel.ISupportInitialize)(this.dataGridView2)).BeginInit();
+            this.menuStrip1.SuspendLayout();
+            this.SuspendLayout();
+            // 
+            // dataGridView1
+            // 
+            this.dataGridView1.BackgroundColor = System.Drawing.SystemColors.MenuBar;
+            this.dataGridView1.ColumnHeadersHeightSizeMode = System.Windows.Forms.DataGridViewColumnHeadersHeightSizeMode.AutoSize;
+            this.dataGridView1.GridColor = System.Drawing.Color.Red;
+            this.dataGridView1.Location = new System.Drawing.Point(74, 68);
+            this.dataGridView1.Name = "dataGridView1";
+            this.dataGridView1.Size = new System.Drawing.Size(40, 40);
+            this.dataGridView1.TabIndex = 0;
+            // 
+            // dataGridView2
+            // 
+            this.dataGridView2.BackgroundColor = System.Drawing.SystemColors.MenuBar;
+            this.dataGridView2.CellBorderStyle = System.Windows.Forms.DataGridViewCellBorderStyle.Sunken;
+            this.dataGridView2.ColumnHeadersHeightSizeMode = System.Windows.Forms.DataGridViewColumnHeadersHeightSizeMode.AutoSize;
+            this.dataGridView2.GridColor = System.Drawing.Color.Red;
+            this.dataGridView2.Location = new System.Drawing.Point(110, 68);
+            this.dataGridView2.Name = "dataGridView2";
+            this.dataGridView2.Size = new System.Drawing.Size(40, 40);
+            this.dataGridView2.TabIndex = 1;
+            // 
+            // menuStrip1
+            // 
+            this.menuStrip1.Items.AddRange(new System.Windows.Forms.ToolStripItem[] {
+            this.threadToolStripMenuItem});
+            this.menuStrip1.Location = new System.Drawing.Point(0, 0);
+            this.menuStrip1.Name = "menuStrip1";
+            this.menuStrip1.Size = new System.Drawing.Size(800, 24);
+            this.menuStrip1.TabIndex = 2;
+            this.menuStrip1.Text = "menuStrip1";
+            // 
+            // threadToolStripMenuItem
+            // 
+            this.threadToolStripMenuItem.DropDownItems.AddRange(new System.Windows.Forms.ToolStripItem[] {
+            this.startToolStripMenuItem,
+            this.pauseToolStripMenuItem,
+            this.resumeToolStripMenuItem});
+            this.threadToolStripMenuItem.Name = "threadToolStripMenuItem";
+            this.threadToolStripMenuItem.Size = new System.Drawing.Size(55, 20);
+            this.threadToolStripMenuItem.Text = "Thread";
+            // 
+            // startToolStripMenuItem
+            // 
+            this.startToolStripMenuItem.Name = "startToolStripMenuItem";
+            this.startToolStripMenuItem.Size = new System.Drawing.Size(116, 22);
+            this.startToolStripMenuItem.Text = "Start";
+            this.startToolStripMenuItem.Click += new System.EventHandler(this.StartToolStripMenuItem_Click);
+            // 
+            // pauseToolStripMenuItem
+            // 
+            this.pauseToolStripMenuItem.Name = "pauseToolStripMenuItem";
+            this.pauseToolStripMenuItem.Size = new System.Drawing.Size(116, 22);
+            this.pauseToolStripMenuItem.Text = "Pause";
+            this.pauseToolStripMenuItem.Click += new System.EventHandler(this.PauseToolStripMenuItem_Click);
+            // 
+            // resumeToolStripMenuItem
+            // 
+            this.resumeToolStripMenuItem.Name = "resumeToolStripMenuItem";
+            this.resumeToolStripMenuItem.Size = new System.Drawing.Size(116, 22);
+            this.resumeToolStripMenuItem.Text = "Resume";
+            this.resumeToolStripMenuItem.Click += new System.EventHandler(this.ResumeToolStripMenuItem_Click);
+            // 
+            // button1
+            // 
+            this.button1.BackColor = System.Drawing.SystemColors.ActiveCaptionText;
+            this.button1.ForeColor = System.Drawing.SystemColors.ControlLightLight;
+            this.button1.Location = new System.Drawing.Point(181, 1);
+            this.button1.Name = "button1";
+            this.button1.Size = new System.Drawing.Size(75, 23);
+            this.button1.TabIndex = 3;
+            this.button1.Text = "Start";
+            this.button1.UseVisualStyleBackColor = false;
+            this.button1.Click += new System.EventHandler(this.ButtonStart);
+            // 
+            // Stop
+            // 
+            this.Stop.BackColor = System.Drawing.SystemColors.ActiveCaptionText;
+            this.Stop.ForeColor = System.Drawing.SystemColors.ControlLightLight;
+            this.Stop.Location = new System.Drawing.Point(284, 0);
+            this.Stop.Name = "Stop";
+            this.Stop.Size = new System.Drawing.Size(75, 23);
+            this.Stop.TabIndex = 4;
+            this.Stop.Text = "Stop";
+            this.Stop.UseVisualStyleBackColor = false;
+            this.Stop.Click += new System.EventHandler(this.ButtonStop);
+            // 
+            // Form1
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.ClientSize = new System.Drawing.Size(800, 450);
+            this.Controls.Add(this.Stop);
+            this.Controls.Add(this.button1);
+            this.Controls.Add(this.dataGridView2);
+            this.Controls.Add(this.dataGridView1);
+            this.Controls.Add(this.menuStrip1);
+            this.MainMenuStrip = this.menuStrip1;
+            this.Name = "Form1";
+            this.Text = "Form1";
+            ((System.ComponentModel.ISupportInitialize)(this.dataGridView1)).EndInit();
+            ((System.ComponentModel.ISupportInitialize)(this.dataGridView2)).EndInit();
+            this.menuStrip1.ResumeLayout(false);
+            this.menuStrip1.PerformLayout();
+            this.ResumeLayout(false);
+            this.PerformLayout();
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.DataGridView dataGridView1;
+        private System.Windows.Forms.DataGridView dataGridView2;
+        private System.Windows.Forms.MenuStrip menuStrip1;
+        private System.Windows.Forms.ToolStripMenuItem threadToolStripMenuItem;
+        private System.Windows.Forms.ToolStripMenuItem startToolStripMenuItem;
+        private System.Windows.Forms.ToolStripMenuItem pauseToolStripMenuItem;
+        private System.Windows.Forms.ToolStripMenuItem resumeToolStripMenuItem;
+        public Thread CounterThread;
+        public bool bThreadStatus;
+        private System.Windows.Forms.Button button1;
+        private System.Windows.Forms.Button Stop;
+    }
+}
+
---
+++ Source/MatrixGrid/Form1.cs
@@ -0,0 +1,130 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+using System.Threading;
+
+namespace MatrixGrid
+{
+    public partial class Form1 : Form
+    {
+
+        //Add the member variables
+        public int m_width; //Width of the grid cell
+
+        public int m_Height; // The height of the Cell
+
+        public int m_Rows; // The Number Of Rows
+
+        public int m_Cols; // The Number Of Columns
+
+        public int m_XOffset; //Offset from which drawing start
+        public int m_YOffset;
+        public int rc = 9;
+        public const int DEFAULT_X_OFFSET = 74;
+        public const int DEFAULT_Y_OFFSET = 68;
+        public const int DEFAULT_NO_ROWS = 2;
+        public const int DEFAULT_NO_COLS = 2;
+        public const int DEFAULT_WIDTH = 40;
+        public const int DEFAULT_HEIGHT = 40;
+        public Form1()
+        {
+            InitializeComponent();
+            bThreadStatus = false;
+            Initialize();
+        }
+
+        public void Initialize()
+        {
+            //Put all the default values
+            m_Rows = DEFAULT_NO_ROWS;
+            m_Cols = DEFAULT_NO_COLS;
+            m_width = DEFAULT_WIDTH;
+            m_Height = DEFAULT_HEIGHT;
+            m_XOffset = DEFAULT_X_OFFSET;
+            m_YOffset = DEFAULT_Y_OFFSET;
+
+        }
+
+        private void StartToolStripMenuItem_Click(object sender, EventArgs e)
+        {
+            CounterThread = new Thread(new ThreadStart(ThreadCounter));
+            CounterThread.Start();
+            bThreadStatus = true;
+        }
+
+        private void PauseToolStripMenuItem_Click(object sender, EventArgs e)
+        {
+            CounterThread.Suspend();
+        }
+
+        private void ResumeToolStripMenuItem_Click(object sender, EventArgs e)
+        {
+            CounterThread.Resume();
+        }
+
+        private void ButtonStop(object sender, EventArgs e)
+        {
+            CounterThread.Suspend();
+        }
+
+        private void ButtonStart(object sender, EventArgs e)
+        {
+            CounterThread = new Thread(new ThreadStart(ThreadCounter));
+            CounterThread.Start();
+            bThreadStatus = true;
+        }
+
+        public void ThreadCounter()
+        {
+
+            Graphics Layout = this.CreateGraphics();
+            Pen layoutPen = new Pen(Color.Red);
+            layoutPen.Width =5;
+            int counter = 2;
+            while (counter <= rc)
+            {
+                Thread.Sleep(500);
+
+                if (counter != rc)
+                {
+                    m_Rows = counter;
+                    m_Cols = counter;
+                    int X = DEFAULT_X_OFFSET;
+                    int Y = DEFAULT_Y_OFFSET;
+                    //Draw rows
+                    for (int i = 0; i <= m_Rows; i++)
+                    {
+                        Layout.DrawLine(layoutPen, X, Y, X + this.m_width * this.m_Cols, Y);
+                        Y += m_Height;
+                    }
+
+                    //Draw Cols
+                    X = DEFAULT_X_OFFSET;
+                    Y = DEFAULT_Y_OFFSET;
+                    for (int j = 0; j <= m_Cols; j++)
+                    {
+                        Layout.DrawLine(layoutPen, X, Y, X, Y + this.m_Height * this.m_Rows);
+                        X += this.m_width;
+                    }
+
+                    counter++;
+                }
+                else
+                {
+                    counter = 2;
+                    Invalidate();
+                }
+
+            }
+
+        }
+       
+
+    }
+}
